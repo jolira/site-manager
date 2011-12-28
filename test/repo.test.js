fea8766bcb1a7f8581de1595dacb0cfc98d0fe76
Joachim Kainz <jfk@jolira.com>
2011-12-28 04:12:27 +0000

    more repo

--- test/repo.test.js
+++ test/repo.test.js
@@ -2,13 +2,13 @@
  * (C) 2012 Jolira; distributed under AGPL 3.0
  *
  */
-var repo = require('../lib/repo');
-var fs = require('fs');
-
-delete process.env.JOLIRA_HOME;
-repo.DEFAULT_NAME = ".smtest";
-
 describe('Repo', function () {
+  var repo = require('../lib/repo');
+  var fs = require('fs');
+
+  delete process.env.JOLIRA_HOME;
+  repo.DEFAULT_NAME = ".smtest";
+
   describe('#load()', function () {
     it('should load without error', function (done) {
       repo.load(function (err) {

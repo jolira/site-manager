8f2d3c1cf27fd25a2c70cc2c2bc476400f544512
Joachim Kainz <jfk@jolira.com>
2011-12-28 04:06:00 +0000

    more repo

--- test/manager.test.js
+++ test/manager.test.js
@@ -2,17 +2,20 @@
  * (C) 2012 Jolira; distributed under AGPL 3.0
  *
  */
+
 var Manager = require('../lib/manager');
 
-describe('Manager', function(){
-  describe('#start()', function(){
-    it('should save without error', function(done){
+describe('Manager', function () {
+  describe('#start()', function () {
+    it('should save without error', function (done) {
       var manager = new Manager(undefined);
 
-      manager.start(function(err){
-        if (err) throw err;
+      manager.start(function (err) {
+        if (err) {
+          throw err;
+        }
         done();
       });
-    })
-  })
-})
+    });
+  });
+});

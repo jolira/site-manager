bbd2c4adf70becf7e7caa4616157130a02e055e7
Joachim Kainz <jfk@jolira.com>
2012-02-08 16:18:36 -0800

    adding the editor

--- test/editor.test.js
+++ test/editor.test.js
@@ -2,6 +2,8 @@
  * (C) 2012 Jolira; distributed under AGPL 3.0
  *
  */
+var editor = require('../lib/editor');
+
 describe('Editor', function () {
   describe('#load()', function () {
     it('should load an emtpy array', function (done) {

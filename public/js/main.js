b3cf43c6e4507d0394aeba5566050cd632e1218f
Joachim Kainz <jfk@jolira.com>
2012-02-22 15:08:42 -0800

    adding a main file that shows how to load zepto and zepto itself.

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+/*
+ * This file should be overriden by a file with the same name in a site.
+ */
+/*jslint browser: true, vars: true, maxerr: 50, indent: 4 */
+/*global require:false */
+require(["libs/zepto-v0.8"], function (zepto) {
+    "use strict";
+
+    console.log(zepto);
+});

14113c9673108c990180b7aadb3a4d7ac639e770
Joachim Kainz <jfk@jolira.com>
2012-02-22 14:37:56 -0800

    add the jolira google id to the example

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,10 +1,12 @@
+/*jslint node: true, vars: true, indent: 4 */
 (function (module) {
-  "use strict";
+    "use strict";
 
-  var path = require("path");
+    var path = require("path");
 
-  module.exports = {
-    "hostname": "simple.jolira.com",
-    "public": path.join(__dirname, "public")
-  };
+    module.exports = {
+        "hostname": "simple.jolira.com",
+        "public": path.join(__dirname, "public"),
+        "GoogleAnalyticsWebPropertyID": "UA-3602945-1"
+    };
 })(module);

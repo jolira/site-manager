f0824886be085966df9c2592b2a0949b2b153a29
Joachim Kainz <jfk@jolira.com>
2012-02-22 15:51:13 -0800

    moved from zepto to jQuery as zepto does not yet support AMD loading and implemented the approach to loading query from require.js documented in http://goo.gl/d9jSW.

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,18 @@
  * This file should be overriden by a file with the same name in a site.
  */
 /*jslint browser: true, vars: true, maxerr: 50, indent: 4 */
-/*global require:false */
-require(["libs/zepto-v0.8"], function (zepto) {
+/*global require:false, console:false */
+
+/**
+ * Load jQuery using the approach documented in http://goo.gl/d9jSW
+ */
+require.config({
+    paths: {
+        "jquery": "libs/jquery-1.7.1"
+    }
+});
+require(["jquery"], function ($) {
     "use strict";
 
-    console.log(zepto);
+    console.log($);
 });

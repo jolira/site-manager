4c26ae14e291f2cbac93a9e0d3a8d3aaa9258530
Gabriel Hopper <gabe@jolira.com>
2012-02-23 16:14:32 -0800

    Use backboneboilerplate's version of backbone and underscore js files to get require to load properly.

--- public/js/main.js
+++ public/js/main.js
@@ -4,20 +4,39 @@
 /*jslint browser: true, vars: true, maxerr: 50, indent: 4 */
 /*global require:false, console:false */
 
-/**
- * Load jQuery using the approach documented in http://goo.gl/d9jSW
- */
-var r = require.config({
+// Require.js allows us to configure shortcut alias
+require.config({
     paths: {
-        "jquery": "libs/jquery-1.7.1",
-        "underscore": 'libs/underscore-1.3.1',
-        "backbone": 'libs/backbone-0.9.1'
+        // Major libraries
+        jquery: 'libs/jquery/jquery-1.7.1',
+        underscore: 'libs/underscore/underscore-min', // https://github.com/amdjs
+        backbone:'libs/backbone/backbone-min', // https://github.com/amdjs
+        //sinon: 'libs/sinon/sinon.js',
+
+        // Require.js plugins
+        text: 'libs/require-1.0.7/text',
+        order: 'libs/require-1.0.7/order'//,
+
+        // Just a short cut so we can put our html outside the js dir
+        // When you have HTML/CSS designers this aids in keeping them out of the js directory
+        //templates: '../templates'
     }
 });
-r(["underscore", "backbone", "jquery"], function ($, _, backbone) {
+
+// Let's kick off the application
+require(["underscore", "backbone", "jquery"], function (_, backbone, $) {
     "use strict";
 
-    console.log($);
     console.log(_);
     console.log(backbone);
+    console.log($);
 });
+
+//require([
+//    'views/app',
+//    'router'
+//], function(AppView, Router){
+//    var appView = new AppView;
+//    appView.render();
+//    Router.initialize();
+//});

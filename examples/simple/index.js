bf0e0cb2146946c0ec982a015a4aa6cb9b7a139e
Joachim Kainz <jfk@jolira.com>
2012-02-27 12:12:46 -0800

    adding html templates

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -2,11 +2,13 @@
 (function (module) {
     "use strict";
 
-    var path = require("path");
+    var path = require("path"),
+        templates = path.join(__dirname, "templates"),
+        pubdir = path.join(__dirname, "public");
 
     module.exports = {
         "hostname": "simple.jolira.com",
-        "public": path.join(__dirname, "public"),
+        "public": pubdir,
         "googleAnalyticsWebPropertyID": "UA-3602945-1",
         "title": "Simple Site-Manager Example",
         "metas": [
@@ -15,6 +17,11 @@
                 "content": "A simple demo for how to use the site-manager " +
                     "(http://github.com/jolira/site-manager)"
             }
+        ],
+        "html-files": [
+            path.join(templates, "header.html"),
+            path.join(templates, "content.html"),
+            path.join(templates, "footer.html")
         ]
     };
 })(module);

8b4d257ad92bc5b11dae4ea15655ac97b1b3b525
Joachim Kainz <jfk@jolira.com>
2012-03-26 06:49:05 -0700

    adding --trace to the build.

--- Jakefile.js
+++ Jakefile.js
@@ -8,7 +8,7 @@
 
     desc('Run tests');
     task('test', [], function () {
-        jake.exec(["./node_modules/.bin/vows"], function () {
+        jake.exec(["./node_modules/.bin/vows --trace"], function () {
             console.log('All tests passed.');
             complete();
         }, {stdout: true});

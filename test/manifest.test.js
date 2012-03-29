6d7740f3edd7790294881c9dd759588f65df9141
Joachim Kainz <jfk@jolira.com>
2012-03-28 23:52:14 -0700

    manifest is now automatically updating

---
+++ test/manifest.test.js
@@ -0,0 +1,44 @@
+/*jslint white: true, forin: false, node: true, indent: 4 */
+(function (module) {
+    "use strict";
+
+    var vows = require('vows'),
+        assert = require('assert'),
+        horaa = require('horaa'),
+        fs = horaa('fs'),
+        manifest = require('../lib/manifest');
+
+    fs.hijack('readFile', function (file, cb) {
+        return cb(undefined, "content");
+    });
+
+    vows.describe('manifest').addBatch({
+        'create the filter': {
+            topic: manifest("/x/manifest.appcache"),
+            'not requesting manifest.appcache': {
+                topic: function (filter) {
+                    filter({}, {}, this.callback);
+                },
+                "" : function (err, result) {
+                    if (err) {
+                        assert.fail();
+                    }
+                }
+            },
+            'requesting manifest.appcache': {
+                topic: function (filter) {
+                    filter({
+                        "url": "/manifest.appcache"
+                    }, {
+
+                    }, this.callback);
+                },
+                "" : function (err, result) {
+                    if (err) {
+                        assert.fail();
+                    }
+                }
+            }
+        }
+    }).export(module);
+})(module);

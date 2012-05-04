0fc45adb1b7a1f996d634063dde1d8c38a6925da
Joachim Kainz <jfk@jolira.com>
2012-05-04 13:22:05 -0700

    using winston for logging and using cluster to launch multiple instances.

--- server.js
+++ server.js
@@ -1,135 +1,72 @@
 (function () {
     "use strict";
 
-    process.on("uncaughtException", function (err) {
-        console.error(err.stack || err);
-    });
+    var logger = require("./lib/debug"),
+        cluster = require('cluster');
 
-    var PORT = 3e3,
-        RESTART_DELAY = 1500,
-        debug = require("./lib/debug"),
-        theLauncher = require('./lib/launcher'),
-        path = require("path"),
-        watch = require("directory-tree-watcher"),
-        watchAll,
-        launcher,
-        sitesDir,
-        port,
-        iscID,
-        iscKey;
+    logger.emerg("test");
 
-    function getSitesDirectory() {
-        if (2 < process.argv.length) {
-            return process.argv[2];
-        }
+    function fork(running) {
+        var child = cluster.fork();
 
-        if (process.env.SITE_MANAGER_DIR) {
-            return process.env.SITE_MANAGER_DIR;
-        }
+        running[child.pid] = true;
 
-        var cwd = process.cwd();
-
-        return path.join(cwd, "sites");
+        return child.pid;
     }
 
-    function getPort() {
-        if (3 < process.argv.length) {
-            return process.argv[3];
+    function shutdown(running) {
+        if (!running) {
+            return;
         }
 
-        if (process.env.SITE_MANAGER_PORT) {
-            return process.env.SITE_MANAGER_PORT;
-        }
+        logger("Shutting down...");
+        var ids = Object.keys(running);
 
-        return PORT;
-    }
-
-    function getISCID() {
-        if (4 < process.argv.length) {
-            return process.argv[4];
-        }
-
-        if (process.env.ISC_ID) {
-            return process.env.ISC_ID;
-        }
-
-        console.error("No ISC ID (DShield user id) specified. Please register " +
-            "at http://goo.gl/NUpFW and specify one using the ISC_ID environment " +
-            "variable.");
-    }
-
-    function getISCKey() {
-        if (5 < process.argv.length) {
-            return process.argv[5];
-        }
-
-        if (process.env.ISC_KEY) {
-            return process.env.ISC_KEY;
-        }
-
-        console.error("No ISC key specified. Please register " +
-            "at http://goo.gl/NUpFW and specify one using the ISC_KEY " +
-            "envrionment variable.");
-    }
-
-    function handleError(err) {
-        if (err) {
-            console.error(err.stack || err);
-            process.exit(-1);
-        }
-    }
-
-    sitesDir = getSitesDirectory();
-    port = getPort();
-    iscID = getISCID();
-    iscKey = getISCKey();
-    launcher = theLauncher(sitesDir);
-
-    function start(){
-        launcher.start(port, iscID, iscKey, function (err) {
-            debug("Started...");
-            handleError(err);
-            watchAll();
+        ids.forEach(function (id) {
+            process.kill(id);
         });
+
+        return;
     }
 
-    function restart() {
-        debug("Restarting...");
-        launcher.stop(function (err) {
-            handleError(err);
-            start();
+    function startMaster() {
+        var signals = process.platform === 'win32' ? ['CTRL_C_EVENT'] : ['SIGINT', 'SIGTERM'],
+            os = require('os'),
+            numCPUs = os.cpus().length,
+            workers = numCPUs * 2,
+            running = {};
+
+        process.on('exit', function () {
+            running = shutdown(running);
         });
-    }
+        signals.forEach(function (signal) {
+            process.on(signal, function () {
+                running = shutdown(running);
+                process.exit();
+            });
+        });
 
-    process.on("uncaughtException", function (exception) {
-        debug("uncaught exception", exception ? exception.stack || exception : null);
-        restart();
-    });
+        // Fork workers.
+        for (var i = 0; i < workers; i++) {
+            var child = fork(running);
+        }
 
-    watchAll = function() {
-        var watcher;
-
-        watch(sitesDir, {
-            except: [".git", ".idea"]
-        }, function(){
-            if (watcher) {
-                watcher.close();
+        cluster.on('death', function (worker) {
+            if (!worker || !running) {
+                return;
             }
 
-            setTimeout(restart, RESTART_DELAY);
-        }, function(err, theWatcher) {
-            watcher = theWatcher;
-        });
-    };
+            delete running[worker.pid];
 
-    var signal = process.platform === 'win32' ? 'CTRL_C_EVENT' : 'SIGINT';
+            var id = fork(running);
 
-    process.on(signal, function () {
-        debug("Shutting down...");
-        launcher.stop(function (err) {
-            handleError(err);
-            process.exit(0);
+            logger('worker %s died; restarted as %s.', worker.pid, id);
         });
-    });
-    start();
+    }
+
+    if (!logger.debug && cluster.isMaster) {
+        return startMaster();
+    }
+
+    require('./lib/server.js');
 })();

73fb9fd35754d8ed47427c34f254b51c3e16148b
Joachim Kainz <jfk@jolira.com>
2012-06-12 15:56:16 -0700

    cleanup of the app-cache handling.

--- public/js/app-cache.js
+++ public/js/app-cache.js
@@ -1,9 +1,50 @@
 /*global PhoneGap:false, require:false */
-(function () {
+(function (window, navigator, console) {
     "use strict";
 
-    var cacheStatusValues = ['uncached', 'idle', 'checking', 'downloading', 'updateready', 'obsolete'],
-        cache = window.applicationCache;
+    // See http://www.html5rocks.com/en/tutorials/cache/beginner
+    var cache = window.applicationCache,
+        app = window["jolira-app"] = window["jolira-app"] || {};
+
+    /**
+     * Override this handler if you would like to handle the update differently
+     */
+    app.applicationCacheUpdateReady = function() {
+        if (confirm('Program updates are available. Load them now?')) {
+            window.location.reload();
+        }
+    };
+
+    function getCacheStatus() {
+        switch (cache.status) {
+            case cache.UNCACHED: // UNCACHED == 0
+                return 'UNCACHED';
+            case cache.IDLE: // IDLE == 1
+                return 'IDLE';
+            case cache.CHECKING: // CHECKING == 2
+                return 'CHECKING';
+            case cache.DOWNLOADING: // DOWNLOADING == 3
+                return 'DOWNLOADING';
+            case cache.UPDATEREADY:  // UPDATEREADY == 4
+                return 'UPDATEREADY';
+            case cache.OBSOLETE: // OBSOLETE == 5
+                return 'OBSOLETE';
+        };
+        return 'UNKNOWN';
+    }
+
+    function logEvent(e) {
+        var type = e.type,
+            status = getCacheStatus(),
+            onLine = (navigator.onLine) ? 'yes' : 'no',
+            message = 'online: ' + onLine + ', event: ' + type + ', status: ' + status;
+
+        if (type === 'error' && navigator.onLine) {
+            message += '; There was an unknown error, check your Cache Manifest.';
+        }
+
+        console.log(message);
+    }
 
     cache.addEventListener('cached', logEvent, false);
     cache.addEventListener('checking', logEvent, false);
@@ -12,31 +53,17 @@
     cache.addEventListener('noupdate', logEvent, false);
     cache.addEventListener('obsolete', logEvent, false);
     cache.addEventListener('progress', logEvent, false);
-    cache.addEventListener('updateready', logEvent, false);
 
-    function logEvent(e) {
-        var onLine = (navigator.onLine) ? 'yes' : 'no',
-            message = 'online: ' + onLine + ', event: ' + e.type +
-            ', status: ' + cacheStatusValues[cache.status];
+    window.addEventListener('load', function(e) {
+        cache.addEventListener('updateready', function (e) {
+            logEvent(e);
 
-        if (type == 'error' && navigator.onLine) {
-            message += ' There was an unknown error, check your Cache Manifest.';
-        }
-
-        console.log(message);
-    }
-
-    cache.addEventListener('updateready', function (e) {
-            // Don't perform "swap" if this is the first cache
-            if (cacheStatusValues[cache.status] != 'idle') {
+            if (cache.status === cache.UPDATEREADY) {
                 cache.swapCache();
 
                 console.log('Swapped/updated the Cache Manifest.');
-
-                if (confirm('Updates to the program are available. Load them?')) {
-                    window.location.reload();
-                }
+                app.applicationCacheUpdateReady();
             }
         }, false);
-    cache.update();
-})();
+    }, false);
+})(window, navigator, console);
